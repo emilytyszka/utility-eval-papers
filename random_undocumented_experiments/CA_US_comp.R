9d60fab58bfce5846ea226542e8ab694e9ba986b
Emily E. Tyszka, MPH <126900346+emilytyszka@users.noreply.github.com>
2026-05-03 12:34:28 -0400

    Starting new branch

--- random_undocumented_experiments/CA_US_comp.R
+++
@@ -1,5 +0,0 @@
-hub_locations %>% filter(geo_type=="state" | abbreviation == "CA") %>% 
-  filter(!(fips %in% c("US", "74", "78"))) %>% 
-  mutate(name = fct_reorder(location_name, population)) -> dat
-  
-ggplot(dat, aes(x = name, y = population)) + geom_col() + facet_wrap(~geo_type)
